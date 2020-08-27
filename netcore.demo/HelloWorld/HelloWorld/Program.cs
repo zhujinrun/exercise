7fb365670c405ec6d389fe8083cfbf97ebaa9ed1
liuzhixin405 <164910441@qq.com>
2020-08-27 23:21:36 +0800

    update

--- netcore.demo/HelloWorld/HelloWorld/Program.cs
+++ netcore.demo/HelloWorld/HelloWorld/Program.cs
@@ -21,19 +21,15 @@ namespace HelloWorld
             //取余分组
             var arrCount = count % len != 0 ? count / len + 1 : count / len;
 
-
             object[] obj0 = new object[arrCount];
             object[] obj1 = new object[arrCount];
             object[] obj2 = new object[arrCount];
             int k = 0;
-            while (count > 0)        //10 /3 =1     9/3 =0      8/3= 2   7/3=  1     6/3 =  0  
+            while (count > 0)        //10--
             {
-
                 if (count % 3 == 0) //0
                 {
-
                     obj0[k] = "test" + count;
-
                 }
                 else if (count % 3 == 1)    //1
                 {
@@ -44,39 +40,33 @@ namespace HelloWorld
                     obj2[k] = "test" + count;
                     k++;
                 }
-
                 count--;
             }
             ints.Add(obj0);
             ints.Add(obj1);
             ints.Add(obj2);
-
             return ints;
         }
-
 
         static void Main(string[] args)
         {
             List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var result = ThreeProcessPartial(list, 3);
-            int i = 1;
-            i = ShowResult(result, i);
+            var result = ThreeProcessPartial(list, 3);         
+            ShowResult(result);
             Console.Read();
         }
 
-        private static int ShowResult(List<object[]> result, int i)
+        private static void ShowResult(List<object[]> result)
         {
+            int i = 1;
             foreach (object[] item in result)
             {
-                Console.WriteLine($"..........第{i}组.........");
+                Console.WriteLine($"..........第{i++}组.........");
                 foreach (var sub in item)
                 {
                     Console.WriteLine(sub);
                 }
-                i++;
             }
-
-            return i;
         }
     }
 }
