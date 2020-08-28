5c4b4cfac50a69bd91059a45022f6090be1d2126
liuzhixin405 <164910441@qq.com>
2020-08-28 09:57:05 +0800

    update

--- netcore.demo/HelloWorld/HelloWorld/Program.cs
+++ netcore.demo/HelloWorld/HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -14,6 +15,12 @@ namespace HelloWorld
 
     class Program
     {
+        /// <summary>
+        /// 版本一
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
         static List<object[]> ThreeProcessPartial(List<int> lists, int len)
         {
             int count = lists.Count; //已知数组个数
@@ -47,15 +54,52 @@ namespace HelloWorld
             ints.Add(obj2);
             return ints;
         }
+        /// <summary>
+        /// 版本二
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        static IEnumerable<object> GetResult(List<int> list,Func<int,bool> func)
+        {
+            List<object> result = new List<object>();
+            int count = list.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (func(i))
+                {
+                    result.Add(list[i]);
+                }
+            }
+            return result;
+        }
 
         static void Main(string[] args)
         {
-            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            var result = ThreeProcessPartial(list, 3);         
-            ShowResult(result);
+            //版本二
+            {
+                List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                List<object[]> result = new List<object[]>();
+                 var count = list.Count;
+                var r1 = GetResult(list,(x)=> x % 3 == 0).ToArray();
+                var r2 = GetResult(list, (x) => x % 3 == 1).ToArray();
+                var r3 = GetResult(list, (x) => x % 3 == 2).ToArray();
+                result.Add(r2); result.Add(r1); result.Add(r3);
+                ShowResult(result);
+            }
+            //版本一
+            {
+                List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                var result = ThreeProcessPartial(list, 3);
+                ShowResult(result);
+            }
+            //未完待续       
             Console.Read();
         }
-
+        /// <summary>
+        /// 展示
+        /// </summary>
+        /// <param name="result"></param>
         private static void ShowResult(List<object[]> result)
         {
             int i = 1;
