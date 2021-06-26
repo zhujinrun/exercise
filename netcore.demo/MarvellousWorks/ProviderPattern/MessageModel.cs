fd0966a9a8ffa6d61430df0cd3bde05bc5e51931
liuzhixin405 <164910441@qq.com>
2021-06-26 21:18:05 +0800

    provider pattern

---
+++ netcore.demo/MarvellousWorks/ProviderPattern/MessageModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderPattern
+{
+    public class MessageModel
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="msg">Message内容</param>
+        /// <param name="pt">Message发布时间</param>
+        public MessageModel(string msg, DateTime pt)
+        {
+            this._message = msg;
+            this._publishTime = pt;
+        }
+
+        private string _message;
+        /// <summary>
+        /// Message内容
+        /// </summary>
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        private DateTime _publishTime;
+        /// <summary>
+        /// Message发布时间
+        /// </summary>
+        public DateTime PublishTime
+        {
+            get { return _publishTime; }
+            set { _publishTime = value; }
+        }
+    }
+}
---
+++ netcore.demo/MarvellousWorks/ProviderPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProviderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Message.Insert(new MessageModel("插入", DateTime.Now)));
+            Console.Write("<br />");
+            Console.Write(Message.Get()[0].Message + " " + Message.Get()[0].PublishTime.ToString());
+            Console.ReadKey();
+        }
+    }
+}
