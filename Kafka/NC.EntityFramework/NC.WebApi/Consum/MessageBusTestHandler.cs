26087e8add860148d19a2478e1fd1b7788633192
liuzhixin405 <164910441@qq.com>
2021-11-01 12:00:23 +0800

    u
    u

--- Kafka/NC.EntityFramework/NC.WebApi/Consum/MessageBusTestHandler.cs
+++ Kafka/NC.EntityFramework/NC.WebApi/Consum/MessageBusTestHandler.cs
@@ -3,15 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace NC.WebApi.MessageBus
 {
     public class MessageBusTestHandler : AbsMessageHandler<Message<string>>
     {
+        BufferBlock<string> _bufferBlock;
+        ActionBlock<string> _actionBlock;
+        public MessageBusTestHandler()
+        {
+            _actionBlock = new ActionBlock<string>(e => PushData(e));
+            _bufferBlock = new BufferBlock<string>() { };
+            _bufferBlock.LinkTo(_actionBlock);
+        }
         public async override Task OnHandle(Message<string> message, string path)
         {
-            Console.WriteLine($"Message Handler TopicName:{path}, Time:{message.CreationDate}, Data:{message.Data}");
-            await Task.CompletedTask;
+                _bufferBlock.Post(message.Data);
+                Console.WriteLine($"Message Handler TopicName:{path}, Time:{message.CreationDate}, Data:{message.Data}");
+                await Task.CompletedTask;
+        }
+        public async Task PushData(string data)
+        {
+            //处理逻辑
+
         }
         /*
         Message Handler TopicName:TopicTestName, Time:2021/7/9 14:17:59, Data:2021/7/9 14:17:58 Producer Test publish.
